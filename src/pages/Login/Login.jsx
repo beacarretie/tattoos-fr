9bbd821fbce474392148e5e535f37dc91aa50d1b
beacarretie <bcarretie@gmail.com>
2024-04-05 13:31:22 +0200

    first commit

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,81 @@
+import { useNavigate } from "react-router-dom";
+import { CustomInput } from "../../components/CustomInput/CustomInput";
+import { ButtonC } from "../../components/ButtonC/ButtonC";
+import { useEffect, useState } from "react";
+import { decodeToken } from "react-jwt";
+import "./Login.css";
+import { loginCall } from "../../services/apiCalls";
+
+export const Login = () => {
+  const navigate = useNavigate();
+
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [msg, setMsg] = useState("");
+
+  const inputHandler = (e) => {
+    //genero la función que bindea
+
+    setCredentials((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const loginMe = async () => {
+    //esta será la función que desencadenará el login...
+    const answer = await loginCall(credentials);
+    if (answer.data.token) {
+      //decodificamos el token...
+      const uDecodificado = decodeToken(answer.data.token);
+
+      const passport = {
+        token: answer.data.token,
+        decodificado: uDecodificado,
+      };
+
+      console.log(passport);
+      //Guardaríamos passport bien en RDX o session/localStorage si no disponemos del primero
+      sessionStorage.setItem("passport", JSON.stringify(passport))
+      
+      setMsg(`${uDecodificado.name}, bienvenid@ de nuevo.`);
+
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    }
+  };
+
+  return (
+    <div className="login-container loginElementsDesign">
+      {msg === "" ? (
+        <>
+          <CustomInput
+            typeProp={"email"}
+            nameProp={"email"}
+            handlerProp={(e) => inputHandler(e)}
+            placeholderProp={"escribe tu e-mail"}
+          />
+          <CustomInput
+            typeProp={"password"}
+            nameProp={"password"}
+            handlerProp={(e) => inputHandler(e)}
+            placeholderProp={"escribe el password"}
+          />
+
+          <ButtonC
+            title={"log me!"}
+            className={"regularButtonClass"}
+            functionEmit={loginMe}
+          />
+        </>
+      ) : (
+        <div>{msg}</div>
+      )}
+      {/* <pre>{JSON.stringify(credentials, null, 2)}</pre> */}
+    </div>
+  );
+};
