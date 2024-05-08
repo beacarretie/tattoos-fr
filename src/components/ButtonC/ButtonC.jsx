9dcd6c2c243d76e2e67c176520c7fa8018fc9481
beacarretie <bcarretie@gmail.com>
2024-05-08 15:37:56 +0200

    admin artist client views

--- src/components/ButtonC/ButtonC.jsx
+++
@@ -1,8 +0,0 @@
-import "./ButtonC.css"
-
-export const ButtonC = ({title, functionEmit, className}) => {
-
-    return(
-        <div className={className} onClick={functionEmit}>{title}</div>
-    )
-}
